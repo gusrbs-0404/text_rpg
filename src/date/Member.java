44c11f97b2f7991facaac3577768c932cefb975b
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-18 12:33:31 +0900

    Add date Pacjage

---
+++ src/date/Member.java
@@ -0,0 +1,8 @@
+package date;
+
+import java.util.ArrayList;
+
+public class Member {
+	public static ArrayList<String> ids = new ArrayList<>();
+	public static ArrayList<String> pws = new ArrayList<>();
+}
