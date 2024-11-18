21991b986efa777e4ed6029bab1cea6cd0f6eb32
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-18 11:36:57 +0900

    Update text_rpg Package - getInstance()

--- src/text_rpg/Main.java
+++ src/text_rpg/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		TextRPG game = TextRpg.get.getInstance();
+		TextRPG game = TextRPG.getInstance();
 		game.run();
 	}
 
--- src/text_rpg/TextRPG.java
+++ src/text_rpg/TextRPG.java
@@ -2,4 +2,16 @@ package text_rpg;
 
 public class TextRPG {
 
+	private void TextRPG() {
+	}
+
+	private static TextRPG instance = new TextRPG();
+
+	public static TextRPG getInstance() {
+		return instance;
+	}
+	
+	public void run() {
+		System.out.println("asdasd");
+	}
 }
