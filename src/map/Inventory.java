0c75c40fe7404f04a5e4d9063433454067276e8e
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-18 13:59:43 +0900

    Update Inventory 기본 틀만 만듬

--- src/map/Inventory.java
+++ src/map/Inventory.java
@@ -2,10 +2,51 @@ package map;
 
 public class Inventory extends Map implements Runnable {
 
+	private boolean isInventory = true;
+
 	@Override
 	public void run() {
+		while (isInventory) {
+			try {
+				System.out.println("=====[INVENTORY]=====");
+				System.out.println("[1. 장비] [2. 가방] [3. 종료]");
+				buffer.setLength(0);
+				System.out.print("메뉴 입력 : ");
+				String select = reader.readLine();
+				menu(select);
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	private void menu(String select) {
+		switch (select) {
+		case "장비":
+			equipment();
+			break;
+		case "가방":
+			inventory();
+			break;
+		case "종료":
+			System.out.println("가방을 닫았습니다.");
+			Thread lobbyThread = new Thread(Lobby.lobby);
+			isInventory = false;
+			lobbyThread.start();
+			break;
+		default:
+			System.err.println("메뉴 잘못 입력했습니다.");
+			break;
+		}
+	}
+
+	private void equipment() {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	private void inventory() {
+		// TODO Auto-generated method stub
+
 	}
 
 }
