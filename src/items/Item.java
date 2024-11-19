63bee9fe86386b93a293b0c619c590f8a4599723
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-19 10:47:58 +0900

    Update Item class 생성자, getName,Attack,Defense,price

--- src/items/Item.java
+++ src/items/Item.java
@@ -1,5 +1,31 @@
 package items;
 
 public class Item {
+	public String name;
+	public int attack;
+	public int defense;
+	public int price;
 
+	public Item(String name, int attack, int defense, int price) {
+		this.name = name;
+		this.attack = attack;
+		this.defense = defense;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public int getDefense() {
+		return defense;
+	}
+
+	public int getPrice() {
+		return price;
+	}
 }
