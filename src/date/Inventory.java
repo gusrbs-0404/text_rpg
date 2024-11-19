336ae929cfa13662660480fbe3a475bd2c3bfe47
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-19 12:24:28 +0900

    Update add, remove, size, print method 추가

--- src/date/Inventory.java
+++ src/date/Inventory.java
@@ -1,9 +1,39 @@
 package date;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 import items.Item;
 
 public class Inventory {
+	public static BufferedWriter wirter = new BufferedWriter(new OutputStreamWriter(System.out));
 	public static ArrayList<Item> inventory = new ArrayList<>();
+
+	public static void add(Item item) {
+		inventory.add(item);
+	}
+
+	public static void remove(Item item) {
+		inventory.remove(item);
+	}
+
+	public static int size() {
+		return inventory.size();
+	}
+
+	public static void printInventory() {
+		for (int i = 0; i < inventory.size(); i++) {
+			input(i + 1 + "번 아이템");
+			input(inventory.get(i));
+		}
+	}
+
+	public static void input(Object object) {
+		try {
+			wirter.append(object + "\n");
+			wirter.flush();
+		} catch (Exception e) {
+		}
+	}
 }
--- src/date/StoreItem.java
+++ src/date/StoreItem.java
@@ -8,25 +8,37 @@ import items.Item;
 public class StoreItem {
 	public static BufferedWriter wirter = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static ArrayList<Item> inventory = new ArrayList<>();
+	public static ArrayList<Item> storeItem = new ArrayList<>();
 
 	public static void add(Item item) {
-		inventory.add(item);
+		storeItem.add(item);
 	}
 
 	public static void remove(Item item) {
-		inventory.remove(item);
+		storeItem.remove(item);
 	}
 
 	public static void clear() {
-		inventory.clear();
+		storeItem.clear();
 	}
 
-	public static void printInventory() {
-		for (int i = 0; i < inventory.size(); i++) {
-			input(i + "1번 아이템");
-			input(inventory.get(i));
+	public static void printStoreItem() {
+		for (int i = 0; i < storeItem.size(); i++) {
+			input(i + 1 + "번 아이템");
+			input(storeItem.get(i));
 		}
+	}
+
+	public static int getPrice(int index) {
+		return storeItem.get(index).price;
+	}
+
+	public static String getName(int index) {
+		return storeItem.get(index).name;
+	}
+
+	public static int size() {
+		return storeItem.size();
 	}
 
 	public static void input(Object object) {
