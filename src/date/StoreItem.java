6b4a2c64f58192294f2e73a44132c402f6c48515
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-19 12:00:24 +0900

    Update StoreItem class add,remove,clear,print 추가

--- src/date/StoreItem.java
+++ src/date/StoreItem.java
@@ -1,10 +1,13 @@
 package date;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-
 import items.Item;
 
 public class StoreItem {
+	public static BufferedWriter wirter = new BufferedWriter(new OutputStreamWriter(System.out));
+
 	public static ArrayList<Item> inventory = new ArrayList<>();
 
 	public static void add(Item item) {
@@ -20,8 +23,17 @@ public class StoreItem {
 	}
 
 	public static void printInventory() {
-		for(int i=0; i<inventory.size(); i++) {
-			inventory.get(i);
+		for (int i = 0; i < inventory.size(); i++) {
+			input(i + "1번 아이템");
+			input(inventory.get(i));
+		}
+	}
+
+	public static void input(Object object) {
+		try {
+			wirter.append(object + "\n");
+			wirter.flush();
+		} catch (Exception e) {
 		}
 	}
 }
