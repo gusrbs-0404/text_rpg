19d46bccf01a0667c8cb16a16ab521536a8aa3fb
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-19 14:41:09 +0900

    아이템 장착 다시하기

--- src/date/PlayerItem.java
+++ src/date/PlayerItem.java
@@ -5,32 +5,29 @@ import java.io.OutputStreamWriter;
 import java.util.HashMap;
 
 import items.Item;
+import units.Unit;
 
 public class PlayerItem {
-	private String name;
-	private HashMap<String, Item> intemMap;
-	
-	public BufferedWriter wirter = new BufferedWriter(new OutputStreamWriter(System.out));
+	private static HashMap<String, Item> intemMap;
 
-	public void equip(Item item) {
+	public static BufferedWriter wirter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void equip(Item item) {
 		intemMap.put(item.getName(), item);
 		input(item.getName() + "이(가) 장착되었습니다.");
 	}
 
-	public void unequip(String itemName) {
-		intemMap.remove(itemName);
-		input(itemName + "이(가) 해제되었습니다.");
+	public static void unequip(Item item) {
+		intemMap.remove(item);
+		input(item.getName() + "이(가) 해제되었습니다.");
 	}
 
-	public void showEquipment() {
-		input(name + "의 장비 목록:");
-		for (Item equipment : intemMap.values()) {
-			input("- " + equipment.getName() + " (공격력: " + equipment.getAttack() + ", 방어력: " + equipment.getDefense()
-					+ ", 가격: " + equipment.getPrice() + ")");
-		}
+	public static void printPlayreItem(Unit unit) {
+		input(unit.name + "의 장비 목록:");
+		
 	}
 
-	public void input(String msg) {
+	public static void input(String msg) {
 		try {
 			wirter.append(msg + "\n");
 			wirter.flush();
--- src/map/Inventory.java
+++ src/map/Inventory.java
@@ -1,5 +1,9 @@
 package map;
 
+import java.util.ArrayList;
+
+import items.Item;
+
 public class Inventory extends Map implements Runnable {
 
 	private boolean isInventory = true;
@@ -39,13 +43,50 @@ public class Inventory extends Map implements Runnable {
 	}
 
 	private void equipment() {
-		// TODO Auto-generated method stub
-
+		date.PlayerItem.printPlayreItem();
 	}
 
 	private void inventory() {
-		// TODO Auto-generated method stub
+		if (date.Inventory.size() == 0) {
+			System.out.println("인벤토리에 아무것도 없다..");
+			return;
+		}
+
+		// 가방에 있는 장비들 보여준다
+		print(date.Inventory.inventory);
+		// 아이템 이름 입력하면 사용, 장착가능
+		try {
+			input("장착할 아이템 번호 입력 : ");
+			String number = reader.readLine();
+			install(number);
+		} catch (Exception e) {
+		}
+	}
+
+	private void install(String number) {
+		// 플레이어마다 착용중인 장비들 따로 출력하기
+		int num = -1;
+		try {
+			num = Integer.parseInt(number) - 1;
+			if (num < 0 || num >= date.Inventory.size()) {
+				input("잘못입력했습니다.");
+			}
+			
+			if(date.Inventory.getItem(num).name.equals("몬스터 정수")) {
+				input("몬스터 정수는 착용 할 수 없습니다.");
+			}
+
+			date.PlayerItem.equip(date.Inventory.getItem(num));
+		} catch (Exception e) {
+			return;
+		}
 
 	}
 
+	private void print(ArrayList<Item> item) {
+		for (int i = 0; i < item.size(); i++) {
+			input(i + 1 + "번 아이템");
+			input(item.get(i));
+		}
+	}
 }
