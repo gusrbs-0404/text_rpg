92df648957f4186e19a6bd924a30931d6d0acc79
gusrbs-0404 <gusrbs7534@gmail.com>
2024-11-23 22:11:49 +0900

    Refactor: Code refactoring Map class 코드 가독성 향상

--- src/map/Map.java
+++ src/map/Map.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Random;
 
 import units.Unit;
@@ -26,6 +27,9 @@ public class Map implements Runnable {
 
 	public int log = -1;
 
+	public static ArrayList<String> ids = date.Member.ids;
+	public static ArrayList<String> pws = date.Member.pws;
+
 	@Override
 	public void run() {
 		isMap = true;
@@ -43,7 +47,8 @@ public class Map implements Runnable {
 
 	private void menu(String select) {
 
-		if (log == -1 && date.Member.ids.size() == 0 && !(select.equals("회원가입") || select.equals("종료") ||select.equals("파일저장") || select.equals("파일로드"))) {
+		if (log == -1 && ids.size() == 0
+				&& !(select.equals("회원가입") || select.equals("종료") || select.equals("파일저장") || select.equals("파일로드"))) {
 			input("회원가입부터 진행해주세요.");
 			return;
 		}
@@ -100,8 +105,8 @@ public class Map implements Runnable {
 			String id = reader.readLine();
 
 			boolean idck = false;
-			for (int i = 0; i < date.Member.ids.size(); i++) {
-				if (date.Member.ids.get(i).equals(id)) {
+			for (int i = 0; i < ids.size(); i++) {
+				if (ids.get(i).equals(id)) {
 					idck = true;
 				}
 			}
@@ -114,8 +119,8 @@ public class Map implements Runnable {
 			input("비밀번호 입력 :");
 			String pw = reader.readLine();
 
-			date.Member.ids.add(id);
-			date.Member.pws.add(pw);
+			ids.add(id);
+			pws.add(pw);
 
 			input("회원가입 완료!");
 		} catch (Exception e) {
@@ -133,7 +138,7 @@ public class Map implements Runnable {
 
 			boolean login = false;
 			for (int i = 0; i < date.Member.ids.size(); i++) {
-				if (date.Member.ids.get(i).equals(id) && date.Member.pws.get(i).equals(pw)) {
+				if (ids.get(i).equals(id) && pws.get(i).equals(pw)) {
 					login = true;
 					log = i;
 				}
@@ -143,7 +148,7 @@ public class Map implements Runnable {
 				input("아이디 또는 비밀번호 잘못입력했습니다.");
 			}
 
-			input(date.Member.ids.get(log) + " 회원님, 로그인 했습니다.");
+			input(ids.get(log) + " 회원님, 로그인 했습니다.");
 		} catch (Exception e) {
 		}
 	}
@@ -161,7 +166,7 @@ public class Map implements Runnable {
 	}
 
 	private void logOut() {
-		input(date.Member.ids.get(log) + " 회원님, 로그어윳 했습니다.");
+		input(ids.get(log) + " 회원님, 로그어윳 했습니다.");
 		log = -1;
 	}
 
@@ -173,8 +178,8 @@ public class Map implements Runnable {
 			String pw = reader.readLine();
 
 			boolean pwck = false;
-			for (int i = 0; i < date.Member.pws.size(); i++) {
-				if (log == i && date.Member.pws.get(i).equals(pw)) {
+			for (int i = 0; i < pws.size(); i++) {
+				if (log == i && pws.get(i).equals(pw)) {
 					pwck = true;
 				}
 			}
@@ -184,9 +189,9 @@ public class Map implements Runnable {
 				return;
 			}
 
-			input(date.Member.ids.get(log) + " 회원님, 회원탈퇴 했습니다.\n");
-			date.Member.ids.remove(log);
-			date.Member.pws.remove(log);
+			input(ids.get(log) + " 회원님, 회원탈퇴 했습니다.\n");
+			ids.remove(log);
+			pws.remove(log);
 			log = -1;
 
 		} catch (Exception e) {
