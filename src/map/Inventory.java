6ae1eebc3f3f2980a5ccaab83f7a064c20282204
gusrbs-0404 <gusrbs7534@gmail.com>
2024-12-01 22:59:06 +0900

    Update Inventory - Update 장비 장착 해제는 되는데, 장비 옵션이 중첩문제

--- src/map/Inventory.java
+++ src/map/Inventory.java
@@ -1,12 +1,14 @@
 package map;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import items.Item;
 import units.Unit;
 
 public class Inventory extends Map implements Runnable {
 	private ArrayList<Unit> party = date.PlayerParty.party;
+	private HashMap<String, Item> equippedItems = units.Unit.equippedItems;
 	private boolean isInventory = true;
 
 	@Override
@@ -49,16 +51,29 @@ public class Inventory extends Map implements Runnable {
 		// 장비 보여주고
 		// 장비 장착 해제할꺼 선택하고
 		// 해제하기
-		
 		try {
 			input("장비 확인할 파티원 선택 : ");
 			String playerNumber = reader.readLine();
+			int playerIndex = getPartyPlIndex(playerNumber);
+			if (playerIndex == -1) {
+				return;
+			}
+			// 장비 장착여부 출력
+			Unit.showStats(playerIndex);
 			
+			input("장비 해제할 아이템 번호  : ");
+			String itemName =  reader.readLine();
 			
+			if(itemName.equals("무기") || itemName.equals("장비")) {
+				// 장비 해제
+				Unit.unequipItem(equippedItems.get(itemName), playerIndex);
+			} else {
+				input("잘못입력했습니다.");
+			}
 		} catch (Exception e) {
 		}
 	}
-	
+
 	private void inventory() {
 		if (date.Inventory.size() == 0) {
 			System.out.println("인벤토리에 아무것도 없다..");
@@ -72,10 +87,10 @@ public class Inventory extends Map implements Runnable {
 			input("장착할 아이템 번호 입력 : ");
 			String itemNumber = reader.readLine();
 			int number = inputNumber(itemNumber);
-			install(number);
-			if(number == -1) {
+			if (number == -1) {
 				return;
 			}
+			install(number);
 		} catch (Exception e) {
 		}
 	}
@@ -86,7 +101,7 @@ public class Inventory extends Map implements Runnable {
 			if (party.size() == 0) {
 				input("파티원이 없습니다.");
 			}
-			
+
 			int num = -1;
 			try {
 				num = Integer.parseInt(number) - 1;
@@ -109,17 +124,19 @@ public class Inventory extends Map implements Runnable {
 			num = Integer.parseInt(number) - 1;
 			if (num < 0 || num >= date.Inventory.size()) {
 				input("잘못입력했습니다.");
+				return -1;
 			}
 
 			if (date.Inventory.getItem(num).name.equals("몬스터 정수")) {
 				input("몬스터 정수는 착용 할 수 없습니다.");
+				return -1;
 			}
 		} catch (Exception e) {
 			return -1;
 		}
 		return num;
 	}
-	
+
 	private void install(int number) {
 		try {
 			printParty();
@@ -128,13 +145,14 @@ public class Inventory extends Map implements Runnable {
 			String playerNumber = reader.readLine();
 			// 파티원 선택
 			int playerIndex = getPartyPlIndex(playerNumber);
-			
+
 			System.out.println(playerIndex);
-			// 장비 장착 
+			// 장비 장착
 			Unit.equipItem(date.Inventory.getItem(number), playerIndex);
+
+			// 장착후 공,방, 장착중인아이템
 			Unit.showStats(playerIndex);
 		} catch (Exception e) {
-			// TODO: handle exception
 		}
 	}
 
